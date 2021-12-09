5c48789cb06272eadb102dd5b3144a5b17e0d947
mv.morzhuhin <mv.morzhuhin@tensor.ru>
2021-12-09 12:40:56 +0300

    edits after the review

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,31 @@
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const form = popup.querySelector('.popup__form');
 const firstInput = popup.querySelector('#author-input');
 const secondInput = popup.querySelector('#about-input');
 const closeButton = popup.querySelector('.popup__close-button');
-const saveButton = popup.querySelector('.popup__button');
 const author = document.querySelector('.profile__title');
 const about = document.querySelector('.profile__subtitle');
-const page = document.querySelector('.page');
 
 function closePopup() {
   popup.classList.remove('popup_active');
-  page.classList.add('page__hidden');
 }
 
-editButton.addEventListener('click', () => {
+function openPopup() {
   firstInput.value = author.textContent;
   secondInput.value = about.textContent;
   popup.classList.add('popup_active');
-  page.classList.add('page__hidden');
-});
+}
 
-closeButton.addEventListener('click', () => {
-  closePopup();
-});
-
-saveButton.addEventListener('click', (evt) => {
+function submitHandler(evt) {
   evt.preventDefault();
   closePopup();
   author.textContent = firstInput.value;
   about.textContent = secondInput.value;
-});
+}
+
+editButton.addEventListener('click', openPopup);
+
+closeButton.addEventListener('click', closePopup);
+
+form.addEventListener('submit', submitHandler);
